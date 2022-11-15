a1b32f575cc62d8e9faf0119875242a5872b7b21
Jake Murphy <jcmurph1228@gmail.com>
2022-11-15 15:54:58 -0800

    Update settings.py

--- workoutapp/workoutapp/settings.py
+++ workoutapp/workoutapp/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-agmj*v^+yq8^!_!d3s!)2u21ox)p(gcwn_55n!!j_j&*go6m2o'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
